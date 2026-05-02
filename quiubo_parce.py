381bd68e7a1ad6aa1073a3c6f0d04c50712cb3c3
Jhon Cardona <benedicto@hotmail.com>
2026-05-01 19:51:27 -0500

    Mi primera app en streamlit

---
+++ quiubo_parce.py
@@ -0,0 +1,13 @@
+
+import streamlit as st
+
+st.title("¡Quiubo Parce!")
+st.subheader("Bienvenido al curso Python para el fin del mundo")
+
+st.write("""
+Esta es mi primera app publicada en Internet.
+Está constriuda en Python con Streamlit.
+""")
+
+st.info("Pronto esta app va a predecir asteroides peligrosos para el planeta Tierra.")
+
